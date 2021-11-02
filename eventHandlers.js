132e9d1ffc7a2a0a6dd2c5b697c08ac8e49ee337
Duska12 <duska.dujakovic@gmx.ch>
2021-11-02 15:33:21 +0100

    deploy

--- eventHandlers.js
+++ eventHandlers.js
@@ -3,6 +3,7 @@ const logger = require('./db/logger.js');
 exports.sendEvent = null;
 
 exports.registerEventHandlers = function (source) {
+    source.addEventListener('MyEvent', handleMyEvent);
 
     // Register more event handlers here
     source.addEventListener('Hantelbewegungen', handleMotionDetection);
@@ -27,6 +28,32 @@ function handleMotionDetection(event) {
 
         // Log the event in the database
         logger.logOne("MyDB", "MotionDetected", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
+function handleMyEvent(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        // you can add more properties to your data object
+        data.myMessage = "Hello World";
+
+        // TODO: do something meaningful with the data
+
+        // Log the event in the database
+        logger.logOne("MyDB", "MyEvent", data);
 
         // send data to all connected clients
         exports.sendEvent(data);

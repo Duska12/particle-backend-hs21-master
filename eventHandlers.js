1aeec4436644b5e22e6fd73c81171b09cd37f6a4
baenztim <baenztim@students.zhaw.ch>
2021-11-02 08:50:42 +0100

    first commit

---
+++ eventHandlers.js
@@ -0,0 +1,38 @@
+const logger = require('./db/logger.js');
+
+exports.sendEvent = null;
+
+exports.registerEventHandlers = function (source) {
+
+    // Register more event handlers here
+    source.addEventListener('Hantelbewegungen', handleMotionDetection);
+}
+
+function handleMotionDetection(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+        // you can add more properties to your data object
+        data.myMessage = "Anzahl Hantelzüge";
+
+        // TODO: do something meaningful with the data
+
+        // Log the event in the database
+        logger.logOne("MyDB", "MotionDetected", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
